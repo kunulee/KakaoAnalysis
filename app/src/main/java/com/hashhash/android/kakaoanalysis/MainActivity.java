fe3473afb4dd881bf5652517a637414cc2776e98
kunulee <kunulee90@gmail.com>
2019-10-14 18:37:59 +0900

    add two image button(develop branch)

--- app/src/main/java/com/hashhash/android/kakaoanalysis/MainActivity.java
+++ app/src/main/java/com/hashhash/android/kakaoanalysis/MainActivity.java
@@ -8,10 +8,14 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     Button button1;
+    ImageButton button_img1;
+    ImageButton button_img2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button)findViewById(R.id.gone);
+        button_img1 = (ImageButton)findViewById(R.id.invisible);
+        button_img2 = (ImageButton)findViewById(R.id.invisible);
     }
 }
