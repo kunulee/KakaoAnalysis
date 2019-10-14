07cf47ddd0ee7667899f184dc39c4131ebc93217
kunulee <kunulee90@gmail.com>
2019-10-14 18:30:20 +0900

    develop image button remove

--- app/src/main/java/com/hashhash/android/kakaoanalysis/MainActivity.java
+++ app/src/main/java/com/hashhash/android/kakaoanalysis/MainActivity.java
@@ -8,12 +8,10 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     Button button1;
-    ImageButton img_button01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button)findViewById(R.id.gone);
-        img_button01 = (Button)findViewById(R.id.button01);
     }
 }
