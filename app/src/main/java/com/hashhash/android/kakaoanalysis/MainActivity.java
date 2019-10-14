1a1a645e4bbe6386655fc2b386bcb5364eb58774
kunulee <kunulee90@gmail.com>
2019-10-14 18:22:54 +0900

    Second Commit - button add - image button add

--- app/src/main/java/com/hashhash/android/kakaoanalysis/MainActivity.java
+++ app/src/main/java/com/hashhash/android/kakaoanalysis/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     Button button1;
+    ImageButton img_button01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button)findViewById(R.id.gone);
+        img_button01 = (Button)findViewById(R.id.button01);
     }
 }
